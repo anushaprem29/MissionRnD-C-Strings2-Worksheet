aecdd507f555709ceea94a8be1a1670aebd43192
anushaprem29 <anushaprem29@missionrnd.com>
2016-03-31 22:42:31 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -10,10 +10,14 @@ Output : 4
 Note: Dont modify original str or word,Just return count ,Spaces can also be part of words like "ab cd" in "ab cd ab cd" returns 2
 */
 
+
+//Using Kmp Algorithm
+
 #include <stdlib.h>
 
 int count_word_in_str_way_1(char *str, char *word){
-	int len = 0, iter = 1, *lps = (int *)malloc(sizeof(int) * 100);
+	int iter = 1, len = 0, count = 0,save;
+	int *lps = (int *)malloc(sizeof(int) * 100);
 	lps[0] = 0;
 	while (word[iter] != '\0'){
 		if (word[iter] == word[len]){
@@ -28,14 +32,15 @@ int count_word_in_str_way_1(char *str, char *word){
 			}
 		}
 	}
-	int lenW = iter, count = 0;
-	for (iter = 0, len = 0; str[iter] != '\0';){
+	len = 0;
+	save = iter;
+	iter = 0;
+	while( str[iter] != '\0'){
 		if (word[len] == str[iter]){
 			len++;
 			iter++;
 		}
-
-		if (len == lenW){
+		if (len == save){
 			count++;
 			len = lps[len - 1];
 		}
